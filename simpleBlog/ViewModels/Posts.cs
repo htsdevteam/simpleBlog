4ca1ad671fa2c56f27cd408b3108578d9ec759f8
serge.work <hts.dev.team@gmail.com>
2017-06-22 21:05:25 +0300

    (29) Worked on frontend Posts controller; implemented all markup for views

---
+++ simpleBlog/ViewModels/Posts.cs
@@ -0,0 +1,25 @@
+﻿using simpleBlog.Infrastructure;
+using simpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simpleBlog.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsShow
+    {
+        public Post Post { get; set; }
+    }
+
+    public class PostsTag
+    {
+        public Tag Tag { get; set; }
+        public PagedData<Post> Posts { get; set; }
+    }
+}
